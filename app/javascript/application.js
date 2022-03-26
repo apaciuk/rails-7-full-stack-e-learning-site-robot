6da762fbc25432551bc2ab8e08bd226c3f27b22a
Paul Anthony McGowan <xhostcom@gmail.com>
2022-03-26 11:01:29 +0000

    Initial commit

---
+++ app/javascript/application.js
@@ -0,0 +1,5 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "bootstrap"
+import 'trix'
+import '@rails/actiontext'
+import "@rails/request.js"
